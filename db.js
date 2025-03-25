1cb5e70e8591e13c8c4a973a11ebbb7fe017a910
Joseph Maxwell <joseph.maxwell@hyland.com>
2025-03-25 09:43:14 -0400

    actually connect to db

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_DB_URI);
+    const conn = await mongoose.connect(process.env.MONGO_DB_URI, { dbName: "hackyformer" });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
